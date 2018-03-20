5c0a6bfd465509bb424bc70e83b74380fb5e18aa
aerth <aerth@riseup.net>
2018-03-20 09:04:35 -0700

    version testing

--- params/version.go
+++ params/version.go
@@ -22,9 +22,9 @@ import (
 
 const (
 	VersionMajor = 1     // Major version component of the current release
-	VersionMinor = 1     // Minor version component of the current release
+	VersionMinor = 3     // Minor version component of the current release
 	VersionPatch = 0     // Patch version component of the current release
-	VersionMeta  = "hf1" // Version metadata to append to the version string
+	VersionMeta  = "testingHF3" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
