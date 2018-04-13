2cc11a3d1a1f9691624cfa9c4b743be92f2155e6
aerth <aerth@riseup.net>
2018-04-13 06:16:00 -0700

    tests: remove fork map

--- tests/init.go
+++ tests/init.go
@@ -18,65 +18,12 @@ package tests
 
 import (
 	"fmt"
-	"math/big"
 
 	"github.com/aquanetwork/aquachain/params"
 )
 
-// This table defines supported forks and their chain config.
-var Forks = map[string]*params.ChainConfig{
-	"Frontier": {
-		ChainId: big.NewInt(1),
-	},
-	"Homestead": {
-		ChainId:        big.NewInt(1),
-		HomesteadBlock: big.NewInt(0),
-	},
-	"EIP150": {
-		ChainId:        big.NewInt(1),
-		HomesteadBlock: big.NewInt(0),
-		EIP150Block:    big.NewInt(0),
-	},
-	"EIP158": {
-		ChainId:        big.NewInt(1),
-		HomesteadBlock: big.NewInt(0),
-		EIP150Block:    big.NewInt(0),
-		EIP155Block:    big.NewInt(0),
-		EIP158Block:    big.NewInt(0),
-	},
-	"Byzantium": {
-		ChainId:        big.NewInt(1),
-		HomesteadBlock: big.NewInt(0),
-		EIP150Block:    big.NewInt(0),
-		EIP155Block:    big.NewInt(0),
-		EIP158Block:    big.NewInt(0),
-		DAOForkBlock:   big.NewInt(0),
-		ByzantiumBlock: big.NewInt(0),
-	},
-	"FrontierToHomesteadAt5": {
-		ChainId:        big.NewInt(1),
-		HomesteadBlock: big.NewInt(5),
-	},
-	"HomesteadToEIP150At5": {
-		ChainId:        big.NewInt(1),
-		HomesteadBlock: big.NewInt(0),
-		EIP150Block:    big.NewInt(5),
-	},
-	"HomesteadToDaoAt5": {
-		ChainId:        big.NewInt(1),
-		HomesteadBlock: big.NewInt(0),
-		DAOForkBlock:   big.NewInt(5),
-		DAOForkSupport: true,
-	},
-	"EIP158ToByzantiumAt5": {
-		ChainId:        big.NewInt(1),
-		HomesteadBlock: big.NewInt(0),
-		EIP150Block:    big.NewInt(0),
-		EIP155Block:    big.NewInt(0),
-		EIP158Block:    big.NewInt(0),
-		ByzantiumBlock: big.NewInt(5),
-	},
-}
+// // This table defines supported forks and their chain config.
+var Forks = map[string]*params.ChainConfig{}
 
 // UnsupportedForkError is returned when a test requests a fork that isn't implemented.
 type UnsupportedForkError struct {
