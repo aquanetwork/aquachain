0885647300c251d87818d77b212685abea85dc61
aerth <aerth@riseup.net>
2018-04-15 15:53:23 -0700

    consensus: add a Light block validator

---
+++ consensus/aquahash/light.go
@@ -0,0 +1,69 @@
+package aquahash
+
+import (
+	"encoding/binary"
+	"fmt"
+	"math/big"
+
+	"github.com/aquanetwork/aquachain/common"
+	"github.com/aquanetwork/aquachain/crypto"
+	"github.com/aquanetwork/aquachain/log"
+	"github.com/aquanetwork/aquachain/params"
+)
+
+var NoMixDigest = common.Hash{}
+
+func NewLight(config *params.ChainConfig) *Light {
+	return &Light{}
+}
+
+type Light struct{}
+type LightBlock interface {
+	Difficulty() *big.Int
+	HashNoNonce() common.Hash
+	Nonce() uint64
+	MixDigest() common.Hash
+	NumberU64() uint64
+	Version() byte
+}
+
+// Verify checks whether the block's nonce is valid.
+func (l *Light) Verify(block LightBlock) bool {
+	// TODO: do aquahash_quick_verify before getCache in order
+	// to prevent DOS attacks.
+
+	if block.Version() != 2 {
+		return false
+	}
+
+	blockNum := block.NumberU64()
+	if blockNum >= epochLength*2048 {
+		log.Debug(fmt.Sprintf("block number %d too high, limit is %d", blockNum, epochLength*2048))
+		return false
+	}
+
+	difficulty := block.Difficulty()
+	/* Cannot happen if block header diff is validated prior to PoW, but can
+		 happen if PoW is checked first due to parallel PoW checking.
+		 We could check the minimum valid difficulty but for SoC we avoid (duplicating)
+	   Ethereum protocol consensus rules here which are not in scope of Aquahash
+	*/
+	if difficulty.Cmp(common.Big0) == 0 {
+		log.Debug("invalid block difficulty")
+		return false
+	}
+
+	// avoid mixdigest malleability as it's not included in a block's "hashNononce"
+	if block.MixDigest() != NoMixDigest {
+		return false
+	}
+
+	seed := make([]byte, 40)
+	copy(seed, block.HashNoNonce().Bytes())
+	binary.LittleEndian.PutUint64(seed[32:], block.Nonce())
+	result := crypto.Argon2id(seed)
+
+	// The actual check.
+	target := new(big.Int).Div(maxUint256, difficulty)
+	return new(big.Int).SetBytes(result).Cmp(target) <= 0
+}
