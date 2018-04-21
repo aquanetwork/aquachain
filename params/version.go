16cee9dff2d6334d8f6fbf068812d9e37061f386
aerth <aerth@riseup.net>
2018-04-20 19:22:07 -0700

    'gofmt -w -l -s .'

--- params/version.go
+++ params/version.go
@@ -21,9 +21,9 @@ import (
 )
 
 const (
-	VersionMajor = 1              // Major version component of the current release
-	VersionMinor = 5              // Minor version component of the current release
-	VersionPatch = 1              // Patch version component of the current release
+	VersionMajor = 1     // Major version component of the current release
+	VersionMinor = 5     // Minor version component of the current release
+	VersionPatch = 1     // Patch version component of the current release
 	VersionMeta  = "HF5" // Version metadata to append to the version string
 )
 
