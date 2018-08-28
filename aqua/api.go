ecdfd9287a15020e0441e0de61957ebd5eb30251
aerth <aerth@riseup.net>
2018-08-28 14:20:15 -0700

    aqua: fix misspell

--- aqua/api.go
+++ aqua/api.go
@@ -92,7 +92,7 @@ func (api *PublicMinerAPI) SubmitWork(nonce types.BlockNonce, solution, digest c
 
 // GetWork returns a work package for external miner. The work package consists of 3 strings
 // result[0], 32 bytes hex encoded current block header pow-hash
-// result[1], 32 bytes hex encoded auxillary hash (pre hf5: dag seed, hf5: zeros, hf8: block number
+// result[1], 32 bytes hex encoded auxiliary hash (pre hf5: dag seed, hf5: zeros, hf8: block number
 // result[2], 32 bytes hex encoded boundary condition ("target"), 2^256/difficulty
 func (api *PublicMinerAPI) GetWork() ([3]string, error) {
 	if !api.e.IsMining() {
