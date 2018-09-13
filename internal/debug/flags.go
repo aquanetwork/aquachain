24b6ff5ce347abccf23e26a54dfa783c975551c8
aerth <aerth@riseup.net>
2018-09-13 13:39:44 -0700

    internal/debug: add "good" and "great" vmodule hints

--- internal/debug/flags.go
+++ internal/debug/flags.go
@@ -40,7 +40,7 @@ var (
 	}
 	vmoduleFlag = cli.StringFlag{
 		Name:  "vmodule",
-		Usage: "Per-module verbosity: comma-separated list of <pattern>=<level> (e.g. aqua/*=5,p2p=4)",
+		Usage: "Per-module verbosity: comma-separated list of <pattern>=<level> (e.g. aqua/*=5,p2p=4), try \"good\" or \"great\" for predefined verbose logging",
 		Value: "",
 	}
 	backtraceAtFlag = cli.StringFlag{
