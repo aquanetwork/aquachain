a2b78dfce37ce685b3eeec5b4954c27eaa91b316
aerth <aerth@riseup.net>
2018-09-20 02:32:36 -0700

    aqua,crypto/secp256k1: ignore testing package with nocgo

--- crypto/secp256k1/secp256_test.go
+++ crypto/secp256k1/secp256_test.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be found in
 // the LICENSE file.
 
+// +build gccgo cgo
+
 package secp256k1
 
 import (
---
+++ crypto/secp256k1/stub_nocgo.go
@@ -0,0 +1,5 @@
+// this page intentionally left blank
+
+// +build !gccgo,!cgo
+
+package secp256k1
