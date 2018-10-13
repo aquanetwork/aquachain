a7eb0278b61284e24fdb02ca4b3173b69808f9b3
aerth <aerth@riseup.net>
2018-10-12 19:21:27 -0700

    tests: gitlab-ci has no user apparently

--- cmd/utils/customflags_test.go
+++ cmd/utils/customflags_test.go
@@ -1,4 +1,5 @@
-// Copyright 2015 The aquachain Authors
+// Copyright 2015 The go-ethereum Authors
+// Copyright 2018 The aquachain Authors
 // This file is part of aquachain.
 //
 // aquachain is free software: you can redistribute it and/or modify
@@ -23,10 +24,14 @@ import (
 )
 
 func TestPathExpansion(t *testing.T) {
-	user, _ := user.Current()
+	var homedir = "~"
+	user, err := user.Current()
+	if err == nil {
+		homedir = user.HomeDir
+	}
 	tests := map[string]string{
 		"/home/someuser/tmp": "/home/someuser/tmp",
-		"~/tmp":              user.HomeDir + "/tmp",
+		"~/tmp":              homedir + "/tmp",
 		"~thisOtherUser/b/":  "~thisOtherUser/b",
 		"$DDDXXX/a/b":        "/tmp/a/b",
 		"/a/b/":              "/a/b",
